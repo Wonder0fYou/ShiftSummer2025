22825d27076b4670878634697e55460dddb61ac0
Dmitry Makeev <dmtdmt2001@gmail.com>
2025-07-01 21:48:13 +0300

    AppNavGraph

---
+++ app/src/main/java/ru/shiftsummer2025/route/AppNavGraph.kt
@@ -0,0 +1,40 @@
+package ru.shiftsummer2025.route
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import ru.shift.authorization_api.route.AuthorizationRouteApi
+import ru.shiftsummer2025.di.AppComponent
+import ru.shiftsummer2025.di.rememberAppDependenciesHolder
+import ru.shiftsummer2025.feature_api.route.RouteApi
+import ru.shiftsummer2025.feature_api.route.registerFeatureGraph
+import kotlin.reflect.KClass
+
+@Composable
+fun AppNavGraph(
+    navHostController: NavHostController,
+    modifier: Modifier,
+    appComponent: AppComponent,
+) {
+    val featureRouteApiFactory = rememberAppDependenciesHolder(
+        appComponent = appComponent
+    ).routeApiFactory.get()
+
+    val routes: List<KClass<out RouteApi>> = buildList {
+        add(AuthorizationRouteApi::class)
+    }
+
+    NavHost(
+        navController = navHostController,
+        startDestination = AuthorizationRouteApi.Screen.BASE
+    ) {
+        routes.forEach { routeKClass ->
+            registerFeatureGraph(
+                routeApi = featureRouteApiFactory.create(routeKClass),
+                navController = navHostController,
+                modifier = modifier
+            )
+        }
+    }
+}
--- app/src/main/java/ru/shiftsummer2025/ui/MainActivity.kt
+++ app/src/main/java/ru/shiftsummer2025/ui/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import ru.shiftsummer2025.ShiftApplication
+import ru.shiftsummer2025.design_system.component.scaffold.ShiftScaffold
 import ru.shiftsummer2025.design_system.theme.ShiftSummer2025Theme
+import ru.shiftsummer2025.route.AppNavGraph
 
 class MainActivity : ComponentActivity() {
 
@@ -22,9 +26,22 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
-            ShiftSummer2025Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            val navController = rememberNavController()
 
+            ShiftSummer2025Theme {
+                ShiftScaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    AppNavGraph(
+                        navHostController = navController,
+                        appComponent = component,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(
+                                bottom = innerPadding.calculateBottomPadding()
+                            )
+                            .imePadding()
+                    )
                 }
             }
         }
