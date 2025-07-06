bd6ef89eba8a26f44911b70e6e3a7522b94848a2
Dmitry Makeev <dmtdmt2001@gmail.com>
2025-07-06 21:07:45 +0300

    поправил ручку + дополнил методы в репозитории + добавил метод в маппер

--- feature/auto/auto-data/src/main/kotlin/ru/shift/auto_data/mapper/CarsMapper.kt
+++ feature/auto/auto-data/src/main/kotlin/ru/shift/auto_data/mapper/CarsMapper.kt
@@ -2,10 +2,12 @@ package ru.shift.auto_data.mapper
 
 import com.skydoves.sandwich.ApiResponse
 import com.skydoves.sandwich.mappers.ApiSuccessModelMapper
+import ru.shift.auto_domain.CarWithRents
 import ru.shift.auto_domain.Cars
 import ru.shift.auto_domain.CarsPaginated
 import ru.shift.auto_domain.MediaCars
 import ru.shift.auto_domain.PaginationMeta
+import ru.shift.cars.model.CarWithRentsDto
 import ru.shift.cars.model.CarsDto
 import ru.shift.cars.model.MediaCarsDto
 import ru.shift.cars.model.PaginationMetaDto
@@ -24,6 +26,20 @@ class CarsMapper @Inject constructor(
         )
         return result
     }
+
+    fun mapToCarWithRents(carWithRentsDto: CarWithRentsDto): CarWithRents =
+        CarWithRents(
+            id = carWithRentsDto.id,
+            name = carWithRentsDto.name,
+            brand = carWithRentsDto.brand,
+            bodyType = carWithRentsDto.bodyType,
+            media = mapToMediaCars(carWithRentsDto.media),
+            transmission = carWithRentsDto.transmission,
+            price = carWithRentsDto.price,
+            location = carWithRentsDto.location,
+            color = carWithRentsDto.color,
+            steering = carWithRentsDto.steering
+        )
 
     private fun mapToCars(carsDto: List<CarsDto>): List<Cars> =
         carsDto.map { carsDto ->
--- feature/auto/auto-data/src/main/kotlin/ru/shift/auto_data/repository/AutoRepositoryImpl.kt
+++ feature/auto/auto-data/src/main/kotlin/ru/shift/auto_data/repository/AutoRepositoryImpl.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import ru.shift.auto_api.repository.AutoRepository
 import ru.shift.auto_data.mapper.CarsMapper
+import ru.shift.auto_domain.CarWithRents
 import ru.shift.auto_domain.CarsPaginated
 import ru.shift.cars.api.CarsRemoteApi
 import ru.shiftsummer2025.feature_api.result.ReasonError
@@ -16,7 +17,7 @@ import javax.inject.Inject
 class AutoRepositoryImpl @Inject constructor(
     private val carsRemoteApi: CarsRemoteApi,
     private val carsMapper: CarsMapper
-) : AutoRepository{
+) : AutoRepository {
     override suspend fun getAllCars(): Flow<Result<CarsPaginated, ReasonError>> = flow {
         val response = carsRemoteApi.getAllCars()
         response.suspendOnSuccess {
@@ -30,6 +31,21 @@ class AutoRepositoryImpl @Inject constructor(
         }.suspendOnError {
             val error = ReasonError(reason = this.message())
             emit(Result.Error(error))
+        }
+    }
+
+    override suspend fun getCarById(carId: String): Flow<Result<CarWithRents, ReasonError>> = flow {
+        val response = carsRemoteApi.getCar(carId)
+        response.suspendOnSuccess {
+            if (data.success) {
+                val car = carsMapper.mapToCarWithRents(this.data.data)
+                emit(Result.Success(car))
+            } else {
+                val error = ReasonError(reason = data.reason)
+                emit(Result.Error(error))
+            }
+        }.suspendOnError {
+
         }
     }
 }
